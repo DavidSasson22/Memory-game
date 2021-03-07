909f5089db8aa74fcde4b9b2d6fa0ed21bf3aff0
DavidSasson22 <75793795+DavidSasson22@users.noreply.github.com>
2021-03-07 19:29:04 +0200

    ---

--- 1/flash-cards-master/src/api/cardsAPI.js
+++
@@ -1,8 +0,0 @@
-// import React from "react";
-import axios from "axios";
-
-const cardsAPI = axios.create({
-  baseURL: "https://5f67855938ce8700163986d4.mockapi.io/cards",
-});
-
-export default cardsAPI;
